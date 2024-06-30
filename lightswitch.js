3191c19ef43b519ac5cb5fc33594642b03c3f744
Lyuying Guo <guolyuying@gmail.com>
2024-06-30 12:43:59 +0800

    lightswitch icon logic

--- lightswitch.js
+++ lightswitch.js
@@ -1,7 +1,18 @@
 function darkMode()
 {
+    // change theme
     var element = document.getElementById("html");
     element.classList.toggle("theme-dark");
 
-    //var icon = document.getElementById("icon");
+    // change theme icon
+    var icon = document.getElementById("icon");
+    if(icon.classList.contains("fa-sun"))
+    {
+        icon.classList.replace("fa-sun","fa-moon");
+    }
+    else
+    {
+        icon.classList.replace("fa-moon","fa-sun");
+    }
+    
 }
