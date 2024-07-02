44d577bddc737583f98820ca561533c27f7dc2d5
Lyuying Guo <guolyuying@gmail.com>
2024-07-02 17:13:13 +0800

    save theme across pages

--- lightswitch.js
+++ lightswitch.js
@@ -1,8 +1,12 @@
+var element = document.getElementById("html");
+
 function darkMode()
 {
     // change theme
-    var element = document.getElementById("html");
     element.classList.toggle("theme-dark");
+
+    // save theme in local storage
+    localStorage.myTheme = element.className;
 
     // change theme icon
     var icon = document.getElementById("icon");
@@ -14,5 +18,12 @@ function darkMode()
     {
         icon.classList.replace("fa-moon","fa-sun");
     }
-    
 }
+
+// if dark mode is saved in local storage, then toggle it on when new page loads, and change icon to moon
+if(localStorage.getItem("myTheme"))
+{
+    element.classList.toggle("theme-dark");
+    icon.classList.replace("fa-sun","fa-moon");
+}
+
